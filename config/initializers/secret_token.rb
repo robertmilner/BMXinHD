d8d35e83c15e9f41082694c60157fae8e7a7e6ca
Robert Milner <dev@rbrtmlnr.com>
2012-03-15 08:27:59 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BMXinHDApp::Application.config.secret_token = '60bff6c807a731862930ea64ddc47aeea2265ad768425d99d4a89390c07c8f14749d3f5f465a84ff3e792ce5ad5eedb007795c5f7abbd46eb2df51696581c32e'
